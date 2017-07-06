4d5d463fdf3e9a81c0d19d49e6d7d8ddc01565ff
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-06 16:24:54 -0400

    light switch button turns on and off

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
+var lightSwitch = document.querySelector('button');
+var on = document.querySelector('.on');
+var off= document.querySelector('.off');
+
+lightSwitch.addEventListener('click', function() {
+  if ( lightSwitch.className === 'switch on' ) {
+    lightSwitch.className = 'switch off';
+  }
+  else { ( lightSwitch.className === 'switch off' )
+  lightSwitch.className = 'switch on';
+}
 
 
+});

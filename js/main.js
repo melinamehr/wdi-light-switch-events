6e544a6e57b667a9b307f67a7879f034f4810ade
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-07 14:17:32 -0400

    assignment complete

--- js/main.js
+++ js/main.js
@@ -1,27 +1,17 @@
 var lightSwitch = document.querySelector('button');
-// var on = document.querySelector('.on');
-// var off = document.querySelector('.off');
-// var light = document.querySelector('.light');
-// var dark = document.querySelector('.dark');
 var body = document.querySelector('body');
-var status = document.querySelector('.status')
+var statustext = document.querySelector('.status')
 
 lightSwitch.addEventListener('click', function() {
   if ( lightSwitch.className === 'switch on' ) {
     body.className = 'light'
     lightSwitch.className = 'switch off';
+    statustext.innerText = "It's so bright in here!";
   }
   else { ( lightSwitch.className === 'switch off' )
   body.className = 'dark';
   lightSwitch.className = 'switch on';
+  statustext.innerText = 'Hey, who turned off the light?';
 }
-
-
-lightSwitch.addEventListener('click', function() {
-  if ( lightSwitch.className === 'switch off' )
-  status.innerText = 'Hey, who turned off the light?'
-
-})
-
 
 });

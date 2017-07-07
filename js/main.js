f541bec03af2451c9614382c8772e57e3ec7375b
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-07 13:51:21 -0400

    got light and dark functions to work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var lightSwitch = document.querySelector('button');
 var on = document.querySelector('.on');
-var off= document.querySelector('.off');
+var off = document.querySelector('.off');
+var light = document.querySelector('.light');
+var dark = document.querySelector('.dark');
+var body = document.querySelector('body');
 
 lightSwitch.addEventListener('click', function() {
   if ( lightSwitch.className === 'switch on' ) {
@@ -9,6 +12,16 @@ lightSwitch.addEventListener('click', function() {
   else { ( lightSwitch.className === 'switch off' )
   lightSwitch.className = 'switch on';
 }
+
+lightSwitch.addEventListener('click', function() {
+  if ( lightSwitch.className === 'switch on') {
+    body.className = 'light'
+  }
+  else { ( lightSwitch.className === 'switch off' )
+  body.className = 'dark';
+}
+
+})
 
 
 });

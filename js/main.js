6ccf61eba6e0c86ff7e6f0b2abe736f8d43ae0ac
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-07-07 14:08:04 -0400

    working on changing the text

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 var lightSwitch = document.querySelector('button');
-var on = document.querySelector('.on');
-var off = document.querySelector('.off');
-var light = document.querySelector('.light');
-var dark = document.querySelector('.dark');
+// var on = document.querySelector('.on');
+// var off = document.querySelector('.off');
+// var light = document.querySelector('.light');
+// var dark = document.querySelector('.dark');
 var body = document.querySelector('body');
+var status = document.querySelector('.status')
 
 lightSwitch.addEventListener('click', function() {
   if ( lightSwitch.className === 'switch on' ) {
+    body.className = 'light'
     lightSwitch.className = 'switch off';
   }
   else { ( lightSwitch.className === 'switch off' )
+  body.className = 'dark';
   lightSwitch.className = 'switch on';
 }
 
+
 lightSwitch.addEventListener('click', function() {
-  if ( lightSwitch.className === 'switch on') {
-    body.className = 'light'
-  }
-  else { ( lightSwitch.className === 'switch off' )
-  body.className = 'dark';
-}
+  if ( lightSwitch.className === 'switch off' )
+  status.innerText = 'Hey, who turned off the light?'
 
 })
 
